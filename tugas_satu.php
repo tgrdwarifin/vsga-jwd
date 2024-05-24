27d9f626ed0e3fb84a86a49b895961172af15065
tgrdwarifin <tgrdwarifin2800@gmail.com>
2024-05-24 19:44:13 +0700

    Add files via upload

---
+++ tugas_satu.php
@@ -0,0 +1,7 @@
+<?php
+echo "Nama : tegar dwi arifin <br>".
+ "Asal Kampus : Universitas Muhammadiyah Kotabumi <br>".
+ "Jurusan : Sistem dan Teknologi Informasi <br>".
+ "Kota : Kotabumi, Lampung Utara, Lampung <br>".
+ "PHP : Pernah <br>".
+ "HTML : Pernah";
